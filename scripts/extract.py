f842539f11e89a70b2f847cf9a97364fbd91a24c
TurinUTDT <ionatan@gmail.com>
2016-01-05 22:28:02 -0300

    Update diario. Deberia agregar algun simbolo al final de la serie de cada cuadrante que indique si convergio ono, y deberia tambien agregar el valor final en angulos.

--- scripts/extract.py
+++ scripts/extract.py
@@ -121,6 +121,7 @@ def loadLevels ():
     import os
     import pandas as pd
 
+    from scripts.settings import makeSettings
     from scripts.general import chkVersion
     chkVersion()
 
@@ -274,10 +275,10 @@ def aplicarFiltros (tabla):
 
     #Filtramos por usuarios si hay alguno determinado
     if filtros['usuario']:
-        if usuario in listaUsuarios.values():
-            tabla = tabla[tabla['Alias']==usuario]
+        if filtros['usuario'] in listaUsuarios.values():
+            tabla = tabla[tabla['Alias']==filtros['usuario']]
         else:
-            tabla = tabla[tabla['userID']==usuario]
+            tabla = tabla[tabla['userID']==filtros['usuario']]
 
     #Filtramos ahora por version del codigo:
     if not filtros['codeVersion'] == 0:
@@ -322,45 +323,3 @@ def estetizarTabla (tabla, nombreTabla):
         if column in filtros[nombreTabla]:
             tabla.drop([column],inplace=True,axis=1)
     return tabla
-
-
-def makeSettings (basic=True,levels=False):
-
-    import json
-
-    filtros = {}
-
-    filtros['sessions']=[]
-    filtros['levels']=[]
-    filtros['trials']=[]
-    filtros['touchs']=[]
-    filtros['sounds']=[]
-
-    if basic:
-        filtros['sessions']=filtros['sessions']+['class','idEnvio','status']
-        filtros['levels']=filtros['levels']+['class','exitTrialId','idEnvio','levelLength','status','trialsVisited','exitTrialPosition','idUser','sortOfTrials','startTrialPosition']
-        filtros['trials']=filtros['trials']+['class','distribucionEnPantalla','idEnvio','indexOfTrialInLevel','resourcesIdSelected','status','trialExitRecorded','trialsInLevel','userId','sessionId','soundLog','touchLog','timeInTrial','timeStopTrialInLevel','resourcesVersion', 0]
-        filtros['touchs']=filtros['touchs']+['levelInstance','numberOfSoundLoops','sessionInstance','soundIdSecuenceInTrial','soundInstance','soundRunning','timeLastStartSound','timeSinceTrialStarts','tipoDeTrial','trialId','timeLastStopSound']
-        filtros['sounds']=filtros['sounds']+['categorias', 'fromStimuli', 'levelInstance', 'numberOfLoop', 'numberOfSoundInTrial', 'soundSecuenceInTrial', 'startTimeSinceTrial', 'stopByEnd', 'stopByExit', 'stopByUnselect', 'stopTime', 'tipoDeTrial', 'trialId', 'sessionInstance']
-
-    if not levels:
-        filtros['levels']=filtros['levels']+['analisis']
-
-    renames = {}
-
-    renames['sessions'] = {'id':'sessionInstance'}
-    renames['levels'] = {'sessionId':'sessionInstance','timeExit':'timeLevelExit','timeStarts':'timeLevelStarts'}
-    renames['trials'] = {'timeExitTrial':'timeTrialExit','jsonMetaDataRta':'jsonMetaDataEstimulo'}
-    renames['touchs'] = {'categorias':'categoriasTouched'}
-    renames['sounds'] = {'soundId':'soundSourceId'}
-
-    listaUsuarios = {1449588595132:'Ioni2', 1449175277519:'Ioni1', 1449524935330:'Iael', 1450205094190:'RieraPruebas',1450227329559:'Lizaso',1450352899438:'Dario17del12'}
-    filtrosxVersion = {'levelVersion':0, 'resourcesVersion':0, 'codeVersion':0, 'filtrarXUsuarioRegistrado':False, 'usuario':False}
-
-    settings = {}
-    settings['filtros']=filtros
-    settings['renames']=renames
-    settings['listaUsuarios']=listaUsuarios
-    settings['filtrosxVersion']=filtrosxVersion
-
-    json.dump(settings, open("./Settings/settings",'w'))
--- scripts/umbralangulos.py
+++ scripts/umbralangulos.py
@@ -1,10 +1,8 @@
-def plotConvergenciaAngulos (levels):
+def plotConvergenciaAngulos (levels, completos=True):
     """
         Esta función grafica el angulo en funcion del numero de trial para cada uno de los cuadrantes
     """
-
     import matplotlib.pyplot as plt
-    import pandas as pd
     from IPython.display import display
     from scripts.general import fechaLocal
     import json
@@ -12,7 +10,59 @@ def plotConvergenciaAngulos (levels):
     from scripts.general import chkVersion
     chkVersion()
 
+    if completos:
+        levels = levels[levels['levelCompleted']]
+
     for usuario in levels['Alias'].unique():
         display ('Se hara la estadistica del usuario: '+usuario)
         levelsUsuario = levels[levels['Alias']==usuario]
         display ('El usuario '+usuario+' jugo '+str(len(levelsUsuario['sessionInstance'].unique()))+' veces')
+        for session in levelsUsuario['sessionInstance'].unique():
+            levelSession = levelsUsuario[levelsUsuario['sessionInstance']==session]
+            display ('En la session '+ str(fechaLocal(session))+ ' el usuario '+str(usuario)+' jugo '+str(len(levelSession['levelInstance'].unique())) + ' niveles')
+            for level in levelSession['levelInstance'].unique():
+                levelLevel = levelSession[levelSession['levelInstance']==level]
+                # display (levelLevel)
+                levelInfo = levelLevel.iloc[0]
+                analisisJson = levelInfo['analisis']
+
+                # Armamos el grafico
+                fig = plt.figure(figsize=(10,3))
+                ax = fig.add_subplot(111)
+                title = 'Evolucion de la dificultad en funcion del trial \n' + 'Usuario: '+str(usuario) + ' en el nivel ' + str(levelInfo['levelTitle'])+', levelVersion: '+str(levelInfo['levelVersion'])
+                ax.set_title(title, fontsize=10, fontweight='bold')
+                ax.set_xlabel('Numero de trial')
+                ax.set_ylabel('Tita (grados)')
+
+                numeroDeCuadrante=0
+                for cuadrante in analisisJson['cuadrantes']:
+                    numeroDeCuadrante = numeroDeCuadrante + 1
+                    #display(cuadrante)
+                    aciertos=[]
+                    angulosRef=[]
+                    angulos=[]
+                    angulosNivel = []
+                    for historial in cuadrante['historial']:
+                        # Extraemos la lista de aciertos o errores de la info del historial
+                        if historial['acertado']:
+                            aciertos=aciertos+[True]
+                        else:
+                            aciertos=aciertos+[False]
+                        angulos = angulos + [historial['angulo']['angulo']]
+                        angulosRef = angulosRef + [historial['angulo']['anguloRef']]
+                        angulosNivel = angulosNivel + [historial['angulo']['nivel']]
+                    display (aciertos)
+                    display (angulosRef)
+                    display (angulos)
+                    display (angulosNivel)
+                    x = range(len(angulosNivel))
+                    y = angulosNivel
+                    ax.plot(x,y, label="Cuadrante "+str(numeroDeCuadrante))
+                    # marcamos aciertos y errores
+                    x = [i for i in range(len(aciertos)) if aciertos[i]]
+                    y = [angulosNivel[i] for i in range(len(aciertos)) if aciertos[i]]
+                    ax.plot(x,y,'go')
+                    x = [i for i in range(len(aciertos)) if not aciertos[i]]
+                    y = [angulosNivel[i] for i in range(len(aciertos)) if not aciertos[i]]
+                    ax.plot(x,y,'ro')
+                    ax.legend()
