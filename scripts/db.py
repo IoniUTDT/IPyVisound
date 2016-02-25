587fadc376a3b6fbe09cf9a37330d1aabd6c06bd
Ionatan Perez <ionatan@gmail.com>
2016-02-24 21:47:04 -0300

    Creo que termine de hacer y revisar el codigo que grafica las convergencias en el formato nuevo :) Queda hacer que muestre los puntitos xq me parece que no esta bien el alrgoritmo que ajusta la dificultad! Tambien que muestre la desviacion en algun lado.

--- scripts/db.py
+++ scripts/db.py
@@ -32,3 +32,141 @@ def downloadFile ():
         os.rename(filenameTemp,filename)
 
     print ('Donload finish')
+
+
+def join (filename='db.json'):
+
+    from scripts.general import chkVersion
+    chkVersion()
+
+    """
+    Este codigo sirve para ir acumulando los datos brutos tal cual salen de la base datos que se descarga, de forma de poder limpiar y reducir el tamaño del archivo online mas o menos seguido
+    sin perder la coherencia de los datos. Esto es necesario porque el json-server no se banca bien manejar archivos muy grandes (empieza a tener delay) y el volumen de datos que se genera crece rapido.
+
+    La idea es que separa en archivos separados las listas de registros separados por categoria para que despues puedan ser procesados segun corresponda
+    """
+
+    from IPython.display import display
+    import json
+    import os
+    import pickle
+
+    # Transformamos el archivo en un json
+    with open(filename) as data_file:
+        db = json.load(data_file)
+
+    # Seleccionamos los datos
+    data = db['Envio']
+
+    # Buscamos la lista de todas las categorias de envios
+    tiposDeEnvio = set([envio['tipoDeEnvio'] for envio in data])
+
+
+
+    # La info guardada en esta parte esta pensada como una lista de envios segun el tipo
+    for tipoDeEnvio in tiposDeEnvio:
+        filename = './Guardados/db.' + tipoDeEnvio
+        enviosNuevos = [envio for envio in data if envio['tipoDeEnvio'] == tipoDeEnvio]
+
+        if os.path.isfile(filename):
+            with open(filename, 'rb') as f:
+                enviosViejos = pickle.load(f)
+            enviosExists = True
+            display (tipoDeEnvio + ' tiene '+str(len(enviosViejos))+' entradas.')
+        else:
+            display ('Warning: no se encontro entradas previas para ' + tipoDeEnvio + ' se guardaran ' + str(len(enviosNuevos)) + ' entradas nuevas.')
+            enviosExists = False
+
+        if enviosExists:
+            enviosUnificados = enviosViejos + [envioNuevo for envioNuevo in enviosNuevos if not envioNuevo['instance'] in set([envioViejo['instance'] for envioViejo in enviosViejos])]
+        else:
+            enviosUnificados = enviosNuevos
+
+        display (tipoDeEnvio + ' paso a tener '+str(len(enviosUnificados))+' entradas.')
+
+        with open(filename, 'wb+') as f:
+            pickle.dump(enviosUnificados, f)
+
+    updateUsers()
+
+def updateUsers():
+
+    from IPython.display import display
+    import os
+    import pickle
+    import json
+
+
+
+    filename = './Guardados/db.' + 'NEWSESION'
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            sessiones = pickle.load(f)
+    else:
+        display ('ERROR! : No se encontro el archivo ' + filename + ' con el registro de las sessiones.')
+        # return
+
+
+    newUsersId = set([json.loads(session['contenido'])['session']['user']['id'] for session in sessiones])
+
+    filename = './Guardados/db.' + 'Alias'
+
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            alias = pickle.load(f)
+    else:
+        alias = {}
+
+    for aliaKey, alis in alias.items():
+        pass
+
+    usersId = [alia['id'] for aliaKey, alia in alias.items()]
+
+    for newUserId in newUsersId:
+        if not newUserId in usersId:
+            newUser = {}
+            newUser['id'] = newUserId
+            newUser['alias'] = str(newUserId)
+            newUser['ignore'] = False
+            alias[newUserId] = newUser
+
+    with open(filename, 'wb') as f:
+        pickle.dump(alias, f)
+
+def updateUser (userId, newAlias, ignore = False):
+
+    from IPython.display import display
+    import os
+    import pickle
+
+    filename = './Guardados/db.' + 'Alias'
+
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            alias = pickle.load(f)
+    else:
+        display ('ERROR : No se ha encontrado el archivo ' + filename)
+
+    user = alias[userId]
+
+    user['alias'] = newAlias
+    user['ignore'] = ignore
+
+    with open(filename, 'wb') as f:
+        pickle.dump(alias, f)
+
+def listOfUsers ():
+
+    from IPython.display import display
+    import os
+    import pickle
+
+    filename = './Guardados/db.' + 'Alias'
+
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            alias = pickle.load(f)
+    else:
+        display ('ERROR : No se ha encontrado el archivo ' + filename)
+
+    display (alias)
---
+++ scripts/plots.py
@@ -0,0 +1,86 @@
+def plotParalelismos (soloCompletados = True):
+
+    from IPython.display import display
+    from scripts.general import fechaLocal
+    import matplotlib.pyplot as plt
+    import os
+    import pickle
+    import json
+
+    # Cargamos los usuarios
+    filename = './Guardados/db.' + 'Alias'
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            alias = pickle.load(f)
+    else:
+        display ('ERROR : No se ha encontrado el archivo ' + filename)
+
+    # Cargamos los registros de convergencia para umbral paralelismo
+    filename = './Guardados/db.' + 'CONVERGENCIAPARALELISMO'
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            convergencias = pickle.load(f)
+    else:
+        display ('ERROR : No se ha encontrado el archivo ' + filename)
+
+
+    convergenciasByLevelInstance = {}
+
+    for convergencia in convergencias:
+        convergencia = json.loads(convergencia['contenido'])
+
+        # Eliminamos las convergencias no terminadas si corresponde
+        if soloCompletados:
+            if not convergencia['dinamica']['convergenciaFinalizada']:
+                continue
+
+        # agrupamos los pares segun corresponda
+        if convergencia['session']['levelInstance'] in convergenciasByLevelInstance.keys():
+            convergenciasByLevelInstance [convergencia['session']['levelInstance']] = convergenciasByLevelInstance [convergencia['session']['levelInstance']] + [convergencia]
+        else:
+            convergenciasByLevelInstance [convergencia['session']['levelInstance']] = [convergencia]
+
+    for levelInstance in convergenciasByLevelInstance.keys():
+
+        # Armamos el grafico
+        fig = plt.figure(figsize=(20, 10))
+        ax = plt.subplot(111)
+        ax.set_xlabel('Numero de trial')
+        ax.set_ylabel('Delta Tita (grados)')
+
+        for convergencia in convergenciasByLevelInstance[levelInstance] :
+
+            # Extraemos datos del la estructura de datos almacenada
+            experimento = convergencia['session']['expName']
+            usuario = alias[convergencia['session']['session']['user']['id']]['alias']
+            levelInstance = convergencia['session']['levelInstance']
+            dinamica = convergencia['dinamica']['identificador']
+            referencia = convergencia['dinamica']['anguloDeReferencia']
+
+            # Extraemos los puntos a graficar
+            y = [elemento['estimulo']['desviacion'] for elemento in convergencia['dinamica']['historial']]
+            x = range (len(y))
+
+            # Armamos el grafico
+            ax.set_xlabel('Numero de trial')
+            ax.set_ylabel('Delta Tita (grados)')
+
+            # Graficamos
+            ax.plot(x,y,label=dinamica)
+
+
+            # Agregamos el punto finalo que marca la convergencia alcanzada
+            if convergencia['dinamica']['convergenciaAlcanzada']:
+                # Calculamos el valor medio de la cantidad de estimulos usados en el programa
+                n = convergencia['dinamica']['tamanoVentanaAnalisisConvergencia']
+                mean = sum(y[-n:]) / float(n)
+                ax.plot([len(y)-n-1,len(y)-1],[mean,mean],'r--')
+                ax.plot([len(y)-1],y[-1],'bs', markersize=10)
+            else:
+                ax.plot([len(y)-1],y[-1],'rs', markersize=10)
+
+
+        # Agregamos decorados
+        ax.plot(ax.get_xlim(),[0,0],'r')
+        ax.set_title(experimento + ' angulo de referencia: ' + str(referencia) + '\n' + ' correspondiente a ' + usuario + ' en la fecha ' + str(fechaLocal(levelInstance)), va='bottom')
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
