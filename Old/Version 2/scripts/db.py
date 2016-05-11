66b05e4fb6938672c1610e7a2cc0538f327668dd
Ionatan Perez <ionatan@gmail.com>
2016-05-11 16:29:08 -0300

    Previo a empezar cn experimento aprendizaje y hacer limpieza de cosas viejas

---
+++ Old/Version 2/scripts/db.py
@@ -0,0 +1,336 @@
+def downloadFile ():
+
+    """
+        Esta funcion descarga la base de datos en formato json del servidor y hace una backup del archivo anterior.
+    """
+
+    import datetime
+    import time
+    import urllib
+    import os
+
+    from scripts.general import chkVersion
+    chkVersion()
+
+    url='http://turintur.dynu.com/db'
+    filenameTemp = 'temp.json'
+    filename = 'db.json'
+    timestamp = time.time()
+    st = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    filenameBackup = './backups/' + filename[:-5] + ' backup ' + st + '.json'
+
+    print ('Starting download, please wait')
+
+    # Bajamos el archivo
+    urllib.request.urlretrieve(url, filenameTemp)
+
+
+    # Renombramos el archivo viejo y dejamos el descargado con el nombre que corresponde si se descargo bien
+    if os.path.isfile(filenameTemp):
+        if os.path.isfile(filename):
+            os.rename(filename,filenameBackup)
+        os.rename(filenameTemp,filename)
+
+    print ('Donload finish')
+
+
+def join (filename='db.json'):
+
+    from scripts.general import chkVersion
+    import scripts.constants
+    chkVersion()
+
+    """
+    Este codigo sirve para ir acumulando los datos brutos tal cual salen de la base datos que se descarga, de forma de poder limpiar y reducir el tamaño del archivo online mas o menos seguido
+    sin perder la coherencia de los datos. Esto es necesario porque el json-server no se banca bien manejar archivos muy grandes (empieza a tener delay) y el volumen de datos que se genera crece rapido.
+
+    La idea es que guarda en archivos separados las listas de registros segun la categoria de envio para que despues puedan ser procesados segun corresponda
+    """
+
+    from IPython.display import display
+    import json
+    import os
+    import pickle
+
+    # Transformamos el archivo en un json
+    with open(filename) as data_file:
+        db = json.load(data_file)
+
+    # Seleccionamos los datos
+    data = db['Envio']
+
+    # Buscamos la lista de todas las categorias de envios
+    tiposDeEnvio = set([envio['tipoDeEnvio'] for envio in data])
+
+
+
+    # La info guardada en esta parte esta pensada como una lista de envios segun el tipo
+    for tipoDeEnvio in tiposDeEnvio:
+        filename = './Guardados/db.' + tipoDeEnvio
+        enviosNuevos = [envio for envio in data if envio['tipoDeEnvio'] == tipoDeEnvio]
+
+        # Sacamos el formato json a los envios
+        for envio in enviosNuevos:
+            envio['contenido'] = json.loads(envio['contenido'])
+
+        if os.path.isfile(filename):
+            with open(filename, 'rb') as f:
+                enviosViejos = pickle.load(f)
+            enviosExists = True
+            display (tipoDeEnvio + ' tiene '+str(len(enviosViejos))+' entradas.')
+        else:
+            display ('Warning: no se encontro entradas previas para ' + tipoDeEnvio + ' se guardaran ' + str(len(enviosNuevos)) + ' entradas nuevas.')
+            enviosExists = False
+
+        if enviosExists:
+            enviosUnificados = enviosViejos + [envioNuevo for envioNuevo in enviosNuevos if not envioNuevo['instance'] in set([envioViejo['instance'] for envioViejo in enviosViejos])]
+        else:
+            enviosUnificados = enviosNuevos
+
+        display (tipoDeEnvio + ' paso a tener '+str(len(enviosUnificados))+' entradas.')
+
+        with open(filename, 'wb+') as f:
+            pickle.dump(enviosUnificados, f)
+
+    joinUsers()
+
+def joinUsers():
+
+    """
+        Esta funcion busca los usuarios nuevos en la lista de sessiones guardadas y compara con la lista de usuarios almacenada en la base de datos alias. Si encuentra que alguno no esta pregunta el alias y si ignorarlo o no y lo incluye
+    """
+    from IPython.display import display
+    import os
+    import pickle
+    from scripts.constants import PATHALIAS, PATHSESSSION
+    from scripts.general import fechaLocal
+
+    filename = PATHSESSSION
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            sessiones = pickle.load(f)
+    else:
+        display ('ERROR! : No se encontro el archivo ' + filename + ' con el registro de las sessiones.')
+        return
+
+    newUsersId = set([session['contenido']['userId'] for session in sessiones])
+
+    filename = PATHALIAS
+
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            users= pickle.load(f)
+    else:
+        users = []
+
+    usersId = [user['id'] for user in users]
+
+    for newUserId in newUsersId:
+        if not newUserId in usersId:
+            # Creamos el usuario
+            newUser = {}
+            newUser['id'] = newUserId
+            # Preguntamos datos del usuario
+            display ('Usuario creado el '+str(fechaLocal(newUser['id'])))
+            response = input("Ingrese un alias o enter para continuar:\n")
+            if response != "":
+                newUser['alias'] = response
+            else:
+                newUser['alias'] = str(newUser['id'])
+            response = input("Presiones 'si' para descartar este usuario del procesamiento de datos:\n")
+            if response == "si":
+                newUser['ignore'] = True
+            else:
+                newUser['ignore'] = False
+            users = users + [newUser]
+
+    with open(filename, 'wb') as f:
+        pickle.dump(users, f)
+
+def updateUser (user):
+
+    """
+        Esta funcion sirve para modificar a mano algun usuario en particular
+    """
+    from IPython.display import display
+    import os
+    import pickle
+    from scripts.constants import PATHALIAS
+
+    filename = PATHALIAS
+
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            users = pickle.load(f)
+    else:
+        display ('ERROR : No se ha encontrado el archivo ' + filename)
+    #display(users)
+    #display(type(user))
+    if isinstance(user, int):
+        for eachuser in users:
+            if eachuser['id'] == user:
+                display ("Se va a modificar el usuario: " + eachuser['alias'])
+                response = input("Ingrese el nuevo alias o presione enter para no cambiarlo:")
+                if response != "":
+                    eachuser['alias'] = response
+                display ("Desea que el usuario " + eachuser['alias'] + " sea ignorado.")
+                response = input("(si/NO)")
+                if response == "si":
+                    eachuser['ignore'] = True
+                else:
+                    eachuser['ignore'] = False
+    if isinstance (user , str):
+        for eachuser in users:
+            if eachuser['alias'] == user:
+                display ("Se va a modificar el usuario: " + eachuser['alias'])
+                response = input("Ingrese el nuevo alias o presione enter para no cambiarlo:")
+                if response != "":
+                    eachuser['alias'] = response
+                display ("Desea que el usuario " + eachuser['alias'] + " sea ignorado.")
+                response = input("(si/NO)")
+                if response == "si":
+                    eachuser['ignore'] = True
+                else:
+                    eachuser['ignore'] = False
+
+    with open(filename, 'wb') as f:
+        pickle.dump(users, f)
+
+def listOfUsers ():
+
+    """
+        devuelve la lista de usuarios almacenada en el alias
+    """
+    from IPython.display import display
+    import os
+    import pickle
+    from scripts.constants import PATHALIAS
+
+    filename = PATHALIAS
+
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            users = pickle.load(f)
+    else:
+        display ('ERROR : No se ha encontrado el archivo ' + filename)
+
+    return users
+
+
+def listOfUsersClean ():
+
+    """
+        devuelve la lista de usuarios almacenada en el alias solo si son de los utiles
+    """
+    from IPython.display import display
+
+    users = listOfUsers()
+    usersClean = [user for user in users if user['ignore']==False]
+    return usersClean
+
+def listOfUsersCleanAlias ():
+
+    """
+        devuelve la lista de alias almacenada en el alias solo si son de los utiles
+    """
+    from IPython.display import display
+
+    usersClean = listOfUsersClean()
+    return [user['alias'] for user in usersClean]
+
+def updateListOfUsers ():
+
+    """
+        Detecta que usuarios no tienen alias y propone modificarlo en forma semiautomartica
+    """
+    from IPython.display import display
+    from scripts.general import fechaLocal
+    import os
+    import pickle
+    from scripts.constants import PATHALIAS
+
+    users = listOfUsers()
+    for user in users:
+        if user['alias']==str(user['id']):
+            display ('Usuario creado el '+str(fechaLocal(user['id'])))
+            response = input("Ingrese un alias o enter para continuar:")
+            if response!= "":
+                user['alias'] = response
+            response = input("Presiones 'si' para descartar este usuario del procesamiento de datos")
+            if response=="si":
+                user['ignore'] = True
+
+    filename = PATHALIAS
+    with open(filename, 'wb') as f:
+        pickle.dump(users, f)
+
+def pandasUtilPiloto(completos=True):
+
+    from IPython.display import display
+    from scripts.constants import PATHSESSSION, PATHCONVERGENCIAS
+    import os
+    import pickle
+    import pandas
+
+    # Cargamos los usuarios
+    users = listOfUsers()
+
+    # Cargamos la base de datos de sessiones
+    filename = PATHSESSSION
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            sessions = pickle.load(f)
+    else:
+        display ('ERROR! : No se encontro el archivo ' + filename)
+        return
+
+    # Cargamos la base de datos de convergencias
+    filename = PATHCONVERGENCIAS
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            dinamicas = pickle.load(f)
+    else:
+        display ('ERROR! : No se encontro el archivo ' + filename)
+        return
+
+    # Extraemos la info util de cada tabla
+
+    # Users...
+    users_df = pandas.DataFrame (users)
+    users_df.rename(columns={'id': 'userId'}, inplace=True)
+    users_df = users_df[users_df['ignore']==False]
+
+    # Sessiones...
+    newSessions = []
+    for session in sessions:
+        newSession = {}
+        newSession['sessionInstance'] = session ['contenido']['sessionInstance']
+        newSession['userId'] = session['contenido']['userId']
+        if 'plataforma' in session['contenido'].keys():
+            newSession['plataforma'] = session['contenido']['plataforma']
+        newSessions = newSessions + [newSession]
+    sessions_df = pandas.DataFrame (newSessions)
+
+    df = pandas.merge(users_df, sessions_df, on='userId')
+
+    # Dinamicas...
+    newDinamicas = []
+    for dinamica in dinamicas:
+        newDinamica = {}
+        newDinamica['sessionInstance'] = dinamica['contenido']['expLog']['session']['sessionInstance']
+        newDinamica['levelInstance'] = dinamica['contenido']['expLog']['levelInstance']
+        newDinamica['expName'] = dinamica['contenido']['expLog']['expName']
+        newDinamica['convergenciaAlcanzada'] = dinamica['contenido']['dinamica']['convergenciaAlcanzada']
+        newDinamica['convergenciaFinalizada'] = dinamica['contenido']['dinamica']['convergenciaFinalizada']
+        newDinamica['historial'] = dinamica['contenido']['dinamica']['historial']
+        newDinamica['identificador'] = dinamica['contenido']['dinamica']['identificador']
+        newDinamica['referencia'] = dinamica['contenido']['dinamica']['referencia']
+        newDinamica['tamanoVentanaAnalisisConvergencia'] = dinamica['contenido']['dinamica']['tamanoVentanaAnalisisConvergencia']
+        newDinamica['ultimaSD'] = dinamica['contenido']['dinamica']['ultimaSD']
+        newDinamica['ultimoMEAN'] = dinamica['contenido']['dinamica']['ultimoMEAN']
+        newDinamicas = newDinamicas + [newDinamica]
+    dinamicas_df = pandas.DataFrame(newDinamicas)
+
+    df = pandas.merge(df,dinamicas_df, on='sessionInstance')
+
+    return df
---
+++ Old/Version 2/scripts/plots.py
@@ -0,0 +1,150 @@
+def plotByUser (alias=["Todos"], completado=True, expList=['UmbralAngulosPiloto', 'UmbralParalelismoPiloto']):
+
+    from scripts.db import pandasUtilPiloto
+    from IPython.display import display
+
+    db = pandasUtilPiloto()
+
+    if alias == ["Todos"]:
+        users = db['alias'].unique()
+    else:
+        users = alias
+
+
+    for user in users:
+
+        userDb = db[db['alias']==user]
+        userAlias = userDb.iloc[0]['alias']
+
+        if completado:
+            userDb = userDb[userDb['convergenciaFinalizada']==True]
+
+        exps = userDb['expName'].unique()
+        for exp in exps:
+            if exp in expList:
+                plotConvergenciaVersionPiloto (userDb, exp)
+
+
+def plotConvergenciaVersionPiloto (db, expName, fromStadistics=False):
+
+    from IPython.display import display
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    import numpy as np
+
+    db=db[db['expName']==expName]
+    dbInfo = db.iloc[0]
+    # Armamos el grafico
+    colormap = plt.cm.nipy_spectral #I suggest to use nipy_spectral, Set1,Paired
+    if fromStadistics:
+        fig = plt.figure(figsize=(20, 10))
+    else:
+        fig = plt.figure(figsize=(20, 20))
+    ax = plt.subplot(111)
+    ax.set_xlabel('Numero de trial')
+    ax.set_ylabel('señal (grados)')
+    title = ''
+    if len(db['alias'].unique()) == 1:
+        title = title + ' Usuario: ' + dbInfo['alias']
+    title = title + ' Experimento: ' + expName
+    ax.set_title(title, va='bottom')
+    # Definimos la escala de colores
+    size = len(db['alias'])
+    colors = iter(cm.Paired(np.linspace(0, 1, size)))
+    # inicializamos los levelInstance graficoados
+    levelColors = {}
+    # Procesamos la info de cada convergencia
+    for index, row in db.iterrows():
+        # La desviacion significa cosas diferentes segun el experimento. En el caso de paralelismo significa la desviacion respecto a la refrebcia, es decir que converge a 0
+        # En el caso de los angulos deberia converger a 90 + referencia, por lo que para graficar y que converja todo junto conviene modificar los datos
+        if expName == 'UmbralParalelismoPiloto':
+            y = [elemento['estimulo']['desviacion'] for elemento in row['historial']]
+        if expName == 'UmbralAngulosPiloto':
+            y = [elemento['estimulo']['desviacion']-90-row['referencia'] for elemento in row['historial']]
+        x = range (len(y))
+        # Construimos el label diferentes segun sea un grafico para un solo usuario o para varios
+        label = ''
+        if len(db['alias'].unique()) != 1:
+            label = label + ' Usuario: ' + row['alias']
+        label = label + ' referencia: ' + str(row['referencia'])
+
+        # Graficamos
+        if row['levelInstance'] in levelColors.keys():
+            color = levelColors[row['levelInstance']]
+            ax.plot(x,y,color=color)
+        else:
+            color=next(colors)
+            ax.plot(x,y,label=label,color=color)
+            levelColors[row['levelInstance']] = color
+
+        # Agregamos las marcas
+        aciertos = [elemento['acertado'] for elemento in row['historial']]
+        for i in x:
+            if aciertos[i]:
+                ax.plot(i,y[i],'bo')
+            else:
+                ax.plot(i,y[i],'ro')
+
+        if not fromStadistics:
+            # Agregamos el punto finalo que marca la convergencia alcanzada
+            if row['convergenciaAlcanzada']:
+                # Calculamos el valor medio de la cantidad de estimulos usados en el programa
+                n = row['tamanoVentanaAnalisisConvergencia']
+                mean = sum(y[-n:]) / float(n)
+                ax.plot([len(y)-n-1,len(y)-1],[mean,mean],'r--')
+                ax.plot([len(y)-1],y[-1],'bs', markersize=10)
+            else:
+                ax.plot([len(y)-1],y[-1],'rs', markersize=10)
+
+
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.show(block=False)
+
+"""
+def plotAllByUser (userAlias=[], soloCompletados = True):
+
+
+        # Extraemos datos del la estructura de datos almacenada
+        experimento = convergencia['session']['expName']
+        usuario = alias[convergencia['session']['session']['user']['id']]['alias']
+        levelInstance = convergencia['session']['levelInstance']
+        dinamica = convergencia['dinamica']['identificador']
+        referencia = convergencia['dinamica']['referencia']
+
+        # Extraemos los puntos a graficar
+        y = [elemento['estimulo']['desviacion'] for elemento in convergencia['dinamica']['historial']]
+        x = range (len(y))
+
+        # Armamos el grafico
+        ax.set_xlabel('Numero de trial')
+        ax.set_ylabel('Delta Tita (grados)')
+
+        # Graficamos
+        ax.plot(x,y,label=dinamica)
+
+        aciertos = [elemento['acertado'] for elemento in convergencia['dinamica']['historial']]
+
+        for i in x:
+            if aciertos[i]:
+                ax.plot(i,y[i],'bo')
+            else:
+                ax.plot(i,y[i],'ro')
+
+        # Agregamos el punto finalo que marca la convergencia alcanzada
+        if convergencia['dinamica']['convergenciaAlcanzada']:
+            # Calculamos el valor medio de la cantidad de estimulos usados en el programa
+            n = convergencia['dinamica']['tamanoVentanaAnalisisConvergencia']
+            mean = sum(y[-n:]) / float(n)
+            ax.plot([len(y)-n-1,len(y)-1],[mean,mean],'r--')
+            ax.plot([len(y)-1],y[-1],'bs', markersize=10)
+        else:
+            ax.plot([len(y)-1],y[-1],'rs', markersize=10)
+
+        ax.set_title(experimento + ' angulo de referencia: ' + str(referencia) + '\n' + ' correspondiente a ' + usuario + ' en la fecha ' + str(fechaLocal(levelInstance)), va='bottom')
+
+
+        # Agregamos decorados
+        ax.plot(ax.get_xlim(),[0,0],'r')
+        #ax.set_title(experimento + ' angulo de referencia: ' + str(referencia) + '\n' + ' correspondiente a ' + usuario + ' en la fecha ' + str(fechaLocal(levelInstance)), va='bottom')
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+"""
